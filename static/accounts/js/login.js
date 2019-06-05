dd05af3753fcdc90d334b17b56d0a1de70134ea8
rash <mathiasndasi@gmail.com>
2019-06-05 04:24:47 +0100

    login and nav bar styling

--- static/accounts/js/login.js
+++ static/accounts/js/login.js
@@ -1,13 +1,12 @@
 function waring() {
-    let close = document.getElementsByClassName('closebtn')[0];
-    close.onclick = function () {
-        let parent = document.querySelector('.alert');
-        parent.style.opacity = '0';
-        setTimeout(() => {
-            parent.style.display = "none"
-        }, 600);
-    }
-    console.log("mathais")
+  let close = document.getElementsByClassName("closebtn")[0];
+  close.onclick = function() {
+    let parent = document.querySelector(".alert");
+    parent.style.opacity = "0";
+    setTimeout(() => {
+      parent.style.display = "none";
+    }, 600);
+  };
 }
 
 waring();
